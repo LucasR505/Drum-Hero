8d04aa629781a75a4b4dd9eb78dafb3b03eb4727
tcolao <48988132+tcolao@users.noreply.github.com>
2019-04-10 08:54:56 -0400

    Update notes.js

--- DrumHero-master/DrumHero/js/notes.js
+++ DrumHero-master/DrumHero/js/notes.js
@@ -28,51 +28,46 @@ show draws the notes
 checkPress checks to see if the note is over the fret when clicked and returns false if it is and true if not
 */
   checkPress(fret){
+    if(this == lowSearch()){
+      console.log(lowSearch());
     if(!this.bass){
       if(fret.x == this.x){
-        // if(fret.pressed == true && this.hit == false && (this.y < 400 || this.y > 500)){
-        //   this.missed = true;
-        //   console.log("missbegin");
-        //   health--;
-        //   setTimeout(function() {
-        //     this.missed = false;
-        //   }, 100);
-        //   return false;
-        // }
         if(this.y > 400 && this.y < 500 && fret.pressed == true && this.missed == false){
           this.hit = true;
-          console.log(this.hit);
-            return true;
+          return true;
           }
+        else{
+          console.log("missfront" + this.y);
+          this.missed = true;
+          return false;
         }
       }
-      // else{
-      //   if(fret.x == this.x)
-      //     if((fret.pressed == true && this.hit == false) && (this.y < 400 || this.y > 500)){
-      //       this.missed = true;
-      //       health--;
-      //       setTimeout(function() {
-      //         this.missed = false;
-      //       }, 100);
-      //     }
-      //     else if(this.y > 400 && this.y < 500 && fret.pressed == true && this.missed == false){
-      //       console.log(true);
-      //       this.hit = true;
-      //         return true;
-      //   }
-      //     else return false;
-      //   else return false;
-      // }
     }
+    else{
+      if(fret.x == this.x){
+        if((this.y > 340 && this.y < 440) && fret.pressed == true && this.missed == false){
+          this.hit = true;
+          console.log(this.hit);
+          return true;
+          }
+      else{
+        console.log(this.hit);
+        this.missed = true;
+        return false;
+        }
+      }
+    }
+  }
+}
 
 /*
 checkMiss checks if a note has not been pressed and has moved out of the window, therefore being missed
 */
     checkMiss(){
-      if(this.hit == false && this.y>500){
+      if((this.hit == false && this.missed == false) && this.y>500){
         console.log("missend");
         health--
-        this.hit = true;
+        this.missed = true;
       }
     }
 
